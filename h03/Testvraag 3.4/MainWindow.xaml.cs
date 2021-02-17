64861394930431295b46576dff40a58f416bc93a
Migelle Mertens <migelle.mertens@gmail.com>
2021-02-17 12:57:57 +0100

    h3

---
+++ h03/Testvraag 3.4/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Testvraag_3._4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tekenButton_Click(object sender, RoutedEventArgs e)
+        {
+            Line line1 = new Line()
+            {
+                X1 = 10,
+                Y1 = 10,
+                X2 = 64,
+                Y2 = 10,
+                Stroke = new SolidColorBrush(Colors.Black),
+                StrokeThickness = 4
+
+            };
+
+            paperCanvas.Children.Add(line1);
+
+            Line line2 = new Line()
+            {
+                X1 = 38,
+                Y1 = 10,
+                X2 = 38,
+                Y2 = 60,
+                Stroke = new SolidColorBrush(Colors.Black),
+                StrokeThickness = 4
+            };
+
+            paperCanvas.Children.Add(line2);
+        }
+    }
+}
---
+++ h03/Toepassing 3.6/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Toepassing_3._6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tekenButton_Click(object sender, RoutedEventArgs e)
+        {
+            Brush pink = new SolidColorBrush(Colors.Pink);
+            Brush white = new SolidColorBrush(Colors.White);
+
+            TekenCirkel(200, 200, 10, 10, pink);
+            TekenCirkel(50, 50, 40, 40, white);
+            TekenCirkel(50, 50, 130, 40, white);
+
+            Line lijn = new Line()
+            {
+                X1 = 60,
+                Y1 = 150,
+                X2 = 160,
+                Y2 = 150,
+                Stroke = new SolidColorBrush(Colors.Black),
+                StrokeThickness = 2
+            };
+
+            paperCanvas.Children.Add(lijn);
+        }
+
+        private void TekenCirkel(int width, int height, int mLeft, int mTop, Brush kleur)
+        {
+            Ellipse ellipse = new Ellipse()
+            {
+                Width = width,
+                Height = height,
+                Margin = new Thickness(mLeft, mTop, 0, 0),
+                Fill = kleur
+            };
+
+            paperCanvas.Children.Add(ellipse);
+        }
+    }
+}
