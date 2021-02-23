bd3ef3a442e4276ebc0184409a041ee8a90bdb62
Migelle Mertens <migelle.mertens@gmail.com>
2021-02-23 15:22:14 +0100

    Toepassing 4.8

---
+++ h04/Toepassing 4.8/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Toepassing_4._8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateSerialButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultSerialLabel.Content = "";
+            double r1 = Convert.ToDouble(r1TextBox.Text);
+            double r2 = Convert.ToDouble(r2TextBox.Text);
+
+            double serialResult = r1 + r2;
+
+            resultSerialLabel.Content = $"Serial calculation: {serialResult:f}";
+        }
+
+        private void calculateParallelButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultParallelLabel.Content = "";
+            double r1 = Convert.ToDouble(r1TextBox.Text);
+            double r2 = Convert.ToDouble(r2TextBox.Text);
+
+            double parallelResult = (r1 * r2) / (r1 + r2);
+
+            resultParallelLabel.Content = $"Parallel calculation: {parallelResult:f}";
+        }
+    }
+}
