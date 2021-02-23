0375d69f578492c21b787be4ec720074b894b5b7
Migelle Mertens <migelle.mertens@gmail.com>
2021-02-23 14:16:08 +0100

    Toepassing 4.6

---
+++ h04/Toepassing 4.6/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Toepassing_4._6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            celciusResultLabel.Content = "";
+
+            double fahrenheit = Convert.ToDouble(fahrenheitTextBox.Text);
+            double celcius = (fahrenheit - 32) * 5 / 9;
+
+            celciusResultLabel.Content = $"Temperature in Celcius: {celcius:f}°C";
+        }
+    }
+}
