2dbf552d69ed1235d73807d390fda09de4ba4c55
Migelle Mertens <migelle.mertens@gmail.com>
2021-02-23 15:55:02 +0100

    Toepassing 4.10

---
+++ h04/Toepassing 4.10/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Toepassing_4._10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double b = Convert.ToDouble(startingBalanceTextBox.Text);
+            int n = Convert.ToInt32(amountOfYearsTextBox.Text);
+            double r = Convert.ToDouble(yearlyInterestTextBox.Text);
+
+            double finalBalance = b * (1 + (r / 100.0));
+            finalBalance = Math.Pow(finalBalance, n);
+
+            finalBalanceResultLabel.Content = $"{finalBalance:f}";
+        }
+    }
+}
