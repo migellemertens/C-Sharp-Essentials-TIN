22231fcf9001693aeb71f900a7851e020d34c560
Migelle Mertens <migelle.mertens@gmail.com>
2021-02-23 01:13:29 +0100

    toepassing 4.11

---
+++ h04/Toepassing 4.11/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Toepassing_4._11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = "";
+
+            int number = Convert.ToInt32(numberTextBox.Text);
+            int[] binaryNumber = new int[100];
+            int counter = 0;
+            
+            while(number > 0)
+            {
+                binaryNumber[counter] = number % 2;
+                number = number / 2;
+                counter++;
+            }
+
+            for(int i = counter - 1; i >= 0; i--)
+            {
+                resultLabel.Content += $"{binaryNumber[i]}";
+            }
+
+            
+        }
+    }
+}
