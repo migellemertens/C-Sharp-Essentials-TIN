e6d107e1cb1403ad99fa4fc1d0ae669fb43e2214
Migelle Mertens <migelle.mertens@gmail.com>
2021-02-23 14:35:06 +0100

    Toepassing 4.7

---
+++ h04/Toepassing 4.7/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Toepassing_4._7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            int totalNumberofSeconds = Convert.ToInt32(numberOfSecondsTextBox.Text);
+
+            int hours = totalNumberofSeconds / 3600;
+            int minutes = (totalNumberofSeconds % 3600) / 60;
+            int seconds = totalNumberofSeconds % 60;
+
+            resultLabel.Content = $"H: {hours} M: {minutes} S: {seconds}";
+
+        }
+    }
+}
