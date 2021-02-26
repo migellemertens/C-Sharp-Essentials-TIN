281dc740b82b57356f731ae4375078f750d0c99a
Migelle Mertens <migelle.mertens@gmail.com>
2021-02-26 15:53:15 +0100

    added h05 guts

---
+++ h05/Exercise14/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows;
+
+namespace Exercise14
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
