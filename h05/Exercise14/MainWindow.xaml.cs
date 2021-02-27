f1f9c3331c981011a580b7cfef1111468913bc7a
Migelle Mertens <migelle.mertens@gmail.com>
2021-02-27 18:09:15 +0100

    finished ex14

--- h05/Exercise14/MainWindow.xaml.cs
+++ h05/Exercise14/MainWindow.xaml.cs
@@ -9,5 +9,34 @@ namespace Exercise14
         {
             InitializeComponent();
         }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            int decimalNumber = Convert.ToInt32(numberTextBox.Text);
+            string binaryNumber = ConvertNumberToBinary(decimalNumber);
+            resultLabel.Content = binaryNumber;
+
+        }
+
+        private string ConvertNumberToBinary(int decimalNumber)
+        {
+            string returnValue = "";
+            int[] binaryNumber = new int[100];
+            int counter = 0;
+
+            while (decimalNumber > 0)
+            {
+                binaryNumber[counter] = decimalNumber % 2;
+                decimalNumber = decimalNumber / 2;
+                counter++;
+            }
+
+            for (int i = counter - 1; i >= 0; i--)
+            {
+                returnValue += $"{binaryNumber[i]}";
+            }
+
+            return returnValue;
+        }
     }
 }
