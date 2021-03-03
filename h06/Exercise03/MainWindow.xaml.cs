0fbff7488594267f9a70d19398911379b53a5798
Migelle Mertens <migelle.mertens@gmail.com>
2021-03-03 10:26:04 +0100

    Exercise 03 + test

--- h06/Exercise03/MainWindow.xaml.cs
+++ h06/Exercise03/MainWindow.xaml.cs
@@ -1,12 +1,31 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace Exercise03
 {
+    
     public partial class MainWindow : Window
     {
+        private Random randomNumber = new Random();
+        private int _sum = 0;
+        private double _average = 0;
+        private int _counter = 0;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void generateNumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            int generatedNumber = randomNumber.Next(200, 401);
+
+            _sum += generatedNumber;
+            _counter++;
+            _average = (double)_sum / _counter;
+
+            randomTextBox.Text = $"{generatedNumber}";
+            sumTextBox.Text = $"{_sum}";
+            averageTextBox.Text = $"{_average}";
         }
     }
 }
