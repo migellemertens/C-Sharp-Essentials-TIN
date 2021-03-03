6f0c68f033f831d3dfb670c8ec6bb42f4e21f2a7
Migelle Mertens <migelle.mertens@gmail.com>
2021-03-03 10:25:21 +0100

    Exercise 02

---
+++ h06/Exercise02/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercise02
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int _number = 1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            resultLabel.Content = $"{_number}";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _number++;
+            resultLabel.Content = $"{_number}";
+        }
+    }
+}
