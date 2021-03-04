6a27f20e6bfd376f953cbea4e48c150532a2e3f6
Migelle Mertens <migelle.mertens@gmail.com>
2021-03-04 16:17:11 +0100

    Exercise 05

---
+++ h06/Exercise05/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercise05
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private double _minDepth;
+        private double _maxDepth;
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            minDepthSlider.Minimum = 1;
+            minDepthSlider.Maximum = 3;
+
+            maxDepthSlider.Minimum = 1;
+            maxDepthSlider.Maximum = 8;
+
+            shallowMinLabel.Content = $"{minDepthSlider.Minimum}";
+            shallowMaxLabel.Content = $"{minDepthSlider.Maximum}";
+            deepMinLabel.Content = $"{maxDepthSlider.Minimum}";
+            deepMaxLabel.Content = $"{maxDepthSlider.Maximum}";
+
+            chosenMinDepth.Content = $"{minDepthSlider.Value}";
+            chosenMaxDepth.Content = $"{maxDepthSlider.Value}";
+        }
+
+        private void minDepthSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            _minDepth = minDepthSlider.Value;
+            chosenMinDepth.Content = $"{_minDepth}";
+        }
+
+        private void maxDepthSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            _maxDepth = maxDepthSlider.Value;
+            chosenMaxDepth.Content = $"{_maxDepth}";
+        }
+        
+    }
+}
