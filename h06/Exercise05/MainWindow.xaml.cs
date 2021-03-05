ac85fa93a6be11c33501a901880ab54e795c99b5
Migelle Mertens <migelle.mertens@gmail.com>
2021-03-05 16:55:28 +0100

    added DrawPool method

--- h06/Exercise05/MainWindow.xaml.cs
+++ h06/Exercise05/MainWindow.xaml.cs
@@ -27,6 +27,7 @@ namespace Exercise05
 
         private double _minDepth;
         private double _maxDepth;
+        //private Polygon _pool;
 
         private void Window_Initialized(object sender, EventArgs e)
         {
@@ -43,18 +44,59 @@ namespace Exercise05
 
             chosenMinDepth.Content = $"{minDepthSlider.Value}";
             chosenMaxDepth.Content = $"{maxDepthSlider.Value}";
+
+            //DrawPool();
         }
 
         private void minDepthSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             _minDepth = minDepthSlider.Value;
             chosenMinDepth.Content = $"{_minDepth}";
+            UpdatePool();
         }
 
         private void maxDepthSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             _maxDepth = maxDepthSlider.Value;
             chosenMaxDepth.Content = $"{_maxDepth}";
+            UpdatePool();
+        }
+
+        private void DrawPool(double sliderMin, double sliderMax)
+        {
+            paperCanvas.Children.Clear();
+            int x = 10;
+            int y = 10;
+            int width = 50;
+            double slidermin = sliderMin * 4;
+            double slidermax = sliderMax * 4;
+
+            Polygon pool = new Polygon()
+            {
+                Stroke = new SolidColorBrush(Colors.Black),
+                StrokeThickness = 1,
+                Fill = new SolidColorBrush(Colors.LightSeaGreen),
+            };
+
+            Point point1 = new Point(x,y);
+            Point point2 = new Point((x + width),10);
+            Point point3 = new Point((x + width),(y + (slidermin * 4)));
+            Point point4 = new Point(x,(y + (slidermax * 4)));
+            
+
+            PointCollection newPool = new PointCollection();
+            newPool.Add(point1);
+            newPool.Add(point2);
+            newPool.Add(point3);
+            newPool.Add(point4);
+            pool.Points = newPool;
+
+            paperCanvas.Children.Add(pool);
+        }
+
+        private void UpdatePool()
+        {
+            DrawPool(minDepthSlider.Value, maxDepthSlider.Value);
         }
         
     }
