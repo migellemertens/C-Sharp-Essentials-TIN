ede06e9d3db33d100484084da2b53cf8a025d689
Migelle Mertens <migelle.mertens@gmail.com>
2021-03-08 16:06:24 +0100

    exercise 1

---
+++ h07/Exercise01/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercise01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void randomCardButton_Click(object sender, RoutedEventArgs e)
+        {
+            cardLabel.Content = "";
+
+            string[] kaarttypes = { "Schoppen", "Harten", "Klaveren", "Koeken" };
+            string[] kaartValue = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Boer", "Dame", "Heer", "Aas" };
+
+            Random r = new Random();
+
+            string cardType = kaarttypes[r.Next(0, kaarttypes.Length)];
+            string cardValue = kaartValue[r.Next(0, kaartValue.Length)];
+
+            cardLabel.Content = $"{cardType} {cardValue}";
+
+        }
+    }
+}
