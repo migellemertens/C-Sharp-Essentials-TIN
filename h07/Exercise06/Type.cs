8e79a773e2abd882c8ae887e3bdd79802860095b
Migelle Mertens <migelle.mertens@gmail.com>
2021-03-12 14:05:33 +0100

    exercise 06

---
+++ h07/Exercise06/Type.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise06
+{
+    public enum Type
+    {
+        ROCK = 1,
+        PAPER = 2,
+        SCISSORS = 3
+    }
+}
